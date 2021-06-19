8a7e9ca3d2b21a2519a78a1bf3d6d04b16e6e019
igsrc <ivan.galvagno@gmail.com>
2021-06-19 14:46:49 -0300

    Gets

--- src/server.js
+++ src/server.js
@@ -9,3 +9,18 @@ const app = express();
 //Uses
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+//Gets
+app.get('/stocks', (req, res) => {
+    res.send(database.getStocks())
+})
+
+app.get('/stocks/:id', (req, res) => {
+    const stock = database.saveStock({
+        ticket: req.body.ticket,
+        name: req.body.name,
+        price: req.body.price,
+        yeld: req.body.yeld
+    });
+    res.send(stock)
+})
